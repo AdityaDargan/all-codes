89207909291885f77e8f68389de7947ae23b8d23
Aditya <adityadargan.ad12@gmail.com>
2024-10-05 05:00:57 +0530

    updated some files

---
+++ metacontest/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define ll long long
+using namespace std;
+
+int main(){  
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    ll t;
+    cin>>t;
+    // cout<<"Case #"<<ii<<": YES"<<"\n";
+    for(ll ii=1;ii<=t;ii++){
+        
+    }
+}
